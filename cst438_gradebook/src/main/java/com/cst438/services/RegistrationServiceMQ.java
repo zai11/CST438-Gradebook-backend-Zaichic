4eae7dcd5d0c4f02b63055679f7dfc0b63959381
Zaichic <zaipowell@rocketmail.com>
2022-10-25 21:39:06 -0700

    Added changes for Assignment 4 Part 2

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -5,7 +5,9 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
@@ -43,16 +45,21 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@RabbitListener(queues = "gradebook-queue")
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
-		
-		//TODO  complete this method in homework 4
-		
+		Enrollment e = new Enrollment();
+		e.setStudentEmail(enrollmentDTO.studentEmail);
+		e.setStudentName(enrollmentDTO.studentName);
+		Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
+		if (c == null)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course id not found");
+		e.setCourse(c);
+		enrollmentRepository.save(e);
 	}
 
 	// sender of messages to Registration Service
 	@Override
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
-		 
-		//TODO  complete this method in homework 4
+		System.out.println("Post to registration: " + courseDTO);
+		rabbitTemplate.convertAndSend(courseDTO); 
 		
 	}
 
